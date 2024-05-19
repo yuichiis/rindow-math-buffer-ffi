b77d657bb7cdef9e03f8aeda037bbe8ca0cf84d6
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-05-19 17:05:32 +0900

    a

--- tests/RindowTest/Math/Matrix/Buffer/FFI/BufferTest.php
+++ tests/RindowTest/Math/Matrix/Buffer/FFI/BufferTest.php
@@ -112,8 +112,8 @@ class BufferTest extends TestCase
         $this->assertEquals(NDArray::complex64,$buf->dtype());
         $this->assertEquals(3,count($buf));
         $this->assertEquals(8,$buf->value_size());
-        $this->assertEquals(NDArray::complex64,$buf->dtype());
-        //$buf[1] = [1.5,2.5];
+        //$this->assertEquals(NDArray::complex64,$buf->dtype());
+        $buf[1] = [1.5,2.5];
         //$buf[2] = (object)['real'=>3.5,'imag'=>4.5];
         //$vv = $buf[1];
         //$this->assertEquals(1.5,$vv->real);
